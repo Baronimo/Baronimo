7dbe46d56d391752cd1479336d1798598154eac9
mattr51 <51mattr@gmail.com>
2015-03-24 18:47:16 +0000

    Initial Commit Update to Follow

---
+++ Baronimo/src/com/example/baronimo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.baronimo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+        }
+    
+    //setting buttons for MainActivity Screen
+    Button btnQuick = (Button) findViewById(R.id.btnQuick);
+    Button btnPlan = (Button) findViewById(R.id.btnPlan);
+    
+    //Method for btnQuick layout change
+    public void onClickListener(View v){
+    	setContentView(R.layout.activity_quick_drink);
+    }
+    
+    
+}
