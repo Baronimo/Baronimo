08b6c8bf786427c692ec85f73e2e6924cd7abd97
mattr51 <51mattr@gmail.com>
2015-03-24 20:02:27 +0000

    Class added to hold map method to be called

---
+++ Baronimo/src/com/example/baronimo/MapMethods.java
@@ -0,0 +1,10 @@
+package com.example.baronimo;
+/**
+ * Here we should have a method to call a map which takes arguments,
+ * best to have one method which is overloaded?
+ * @author 40033364
+ *
+ */
+public class MapMethods {
+
+}
