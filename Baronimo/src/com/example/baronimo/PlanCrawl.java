f1502d571c75ac858e69d612e08aa3fb517619e9
mattr51 <51mattr@gmail.com>
2015-04-07 14:27:02 +0100

    adding slidingtabs

--- Baronimo/src/com/example/baronimo/PlanCrawl.java
+++ Baronimo/src/com/example/baronimo/PlanCrawl.java
@@ -1,16 +1,10 @@
-package com.example.baronimo;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 /**
- * This page requires a basic menu
+* This page requires a basic menu
  * Functionality should include
  * Load crawl button placed at the top right
  * Save and start Crawl button placed at top left (deactivated unless two or more bars are in the list)
  *  - Once clicked a message pops up to allow input of a name with save and cancel buttons, once submitted then the user is taken to the Crawl page
- * 
+ *  
  * A list of current bars which can be reordered by drag and drop, removed by a double click
  *  
  * Swipe left to reveal all bars ordered by distance away from current location 
@@ -22,32 +16,4 @@ import android.view.MenuItem;
  * Swipe right to see currently selected bars and route displayed on a map - numbered place holders?
  * 
  * @author Matt
- *
- */
-public class PlanCrawl extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_plan_crawl);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.plan_crawl, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-}
+*/
