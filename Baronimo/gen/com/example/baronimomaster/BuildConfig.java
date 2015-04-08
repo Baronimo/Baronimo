8295234979f0b4d7574b62e998b6385a782a4130
Matt Ralphson <51mattr@gmail.com>
2015-04-09 00:14:38 +0100

    broke something, fixed something, broke something else...

--- Baronimo/gen/com/example/baronimomaster/BuildConfig.java
+++ Baronimo/gen/com/example/baronimomaster/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.baronimo;
+package com.example.baronimomaster;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- Baronimo/src/com/example/baronimo/MainActivity.java
+++ Baronimo/src/com/example/baronimo/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.view.View.OnClickListener;
 import com.example.baronimo.R;
 
+
 public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
